cc428db09d067e2f7c0c952528693f71ca97e6a1
Manuel-Bobadilla <manuelcba@outlook.com>
2023-06-28 16:33:33 -0300

    comment minor correction

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     address = models.CharField(blank=False, null=False, max_length=80, help_text="user home address")
     phone = models.IntegerField(blank=False, null=False, help_text="user phone number")
     email = models.CharField(blank=False, null=False, max_length=100, help_text="email address")
-    #password to do
+    #password todo
 
     def __str__(self):
         return self.name
