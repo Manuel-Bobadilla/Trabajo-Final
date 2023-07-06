c2d062c614f4336de5e7eec53f2b211c4bcbc52c
Manuel-Bobadilla <manuelcba@outlook.com>
2023-07-06 14:57:48 -0300

    campos vehiculo

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from users.models import Volunteer
 
 class Vehicle(models.Model):
     domain = models.CharField(blank=False, null=False, max_length=7)
+    brand = models.CharField(blank=False, null=False, max_length=20)
+    model = models.CharField(blank=False, null=False, max_length=20)
+    proprietary = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.domain
