f6d553be741a6970f2f6699ebb67ab37a7897b81
Manuel-Bobadilla <manuelcba@outlook.com>
2023-07-31 19:30:55 -0300

    migrate to postgres

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Volunteer(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     address = models.CharField(blank=False, null=False, max_length=80, help_text="user home address")
-    phone = models.IntegerField(blank=False, null=False, help_text="user phone number")
+    phone = models.CharField(blank=False, null=False, max_length=20, help_text="user phone number")
 
     def __str__(self):
         return str(self.user)
