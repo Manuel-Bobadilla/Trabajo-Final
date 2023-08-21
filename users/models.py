cbe92b6b0775e91e736ff2d4f16b9cc6542239c9
Manuel-Bobadilla <manuelcba@outlook.com>
2023-08-21 19:59:33 -0300

    modify volunteer model

--- users/models.py
+++ users/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import User
 
 class Volunteer(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    address = models.CharField(blank=False, null=False, max_length=80, help_text="user home address")
-    phone = models.CharField(blank=False, null=False, max_length=20, help_text="user phone number")
+    address = models.CharField(blank=False, null=False, max_length=80, help_text="volunteer's home address")
+    phone = models.CharField(blank=False, null=False, max_length=20, help_text="volunteer's phone number")
+    neighborhood = models.CharField(blank=False, null=False, max_length=80, help_text="volunteer's neighborhood")
+    university = models.CharField(blank=False, null=False, max_length=80, help_text="volunteer's university")
+    university_file = models.CharField(blank=False, null=False, max_length=20, help_text="volunteer's university file")
 
     def __str__(self):
         return str(self.user.first_name + " " + self.user.last_name)
