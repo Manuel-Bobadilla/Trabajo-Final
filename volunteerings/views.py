ee0d0706a8a03f7999def0ae94dca08d05295031
Manuel-Bobadilla <manuelcba@outlook.com>
2023-09-04 19:28:38 -0300

    correction posts displayed per volunteering team

--- volunteerings/views.py
+++ volunteerings/views.py
@@ -10,7 +10,9 @@ def Volunteerings(request):
     return render(request, "volunteerings/volunteerings.html",{"volunteerings":volunteerings,})
 
 def ViewVolunteering(request):
-    posts = ActivitieDetailPage.objects.live().public()
+    volunteering = Volunteering.objects.get(id=request.POST.get("volunteering_id"))
+    posts = ActivitieDetailPage.objects.filter(volunteering=volunteering)
+    print(posts)
     current_date = datetime.date.today()
     if request.user.id:
             user = User.objects.get(id=request.user.id)
