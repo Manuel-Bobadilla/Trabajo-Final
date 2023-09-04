1b28ac1da663ecde695d08fdf97aadb3d23df31e
Manuel-Bobadilla <manuelcba@outlook.com>
2023-09-04 20:25:50 -0300

    modified activity_listing_page.html

--- volunteerings/views.py
+++ volunteerings/views.py
@@ -13,22 +13,27 @@ def ViewVolunteering(request):
     volunteering = Volunteering.objects.get(id=request.POST.get("volunteering_id"))
     posts = ActivitieDetailPage.objects.filter(volunteering=volunteering)
     current_date = datetime.date.today()
+    inscripted = False
     if request.user.id:
-            user = User.objects.get(id=request.user.id)
-            volunteer = Volunteer.objects.filter(user = user)
-            if volunteer:
-                activities = ActivitieDetailPage.objects.filter(volunteers = volunteer[0])
-                vehicles = volunteer[0].vehicles.all()
-                vehicles = vehicles
-                activities = activities
-                volunteer = volunteer[0]
-                return render(request, "activitie/activitie_listing_page.html",{
-                     "posts":posts,
-                     "current_date":current_date,
-                     "vehicles":vehicles,
-                     "activities":activities,
-                     "volunteer":volunteer,
-                })
+        user = User.objects.get(id=request.user.id)
+        volunteer = Volunteer.objects.filter(user = user)
+        volunteeringsOfVolunteer = Volunteering.objects.filter(volunteers__in = volunteer)
+        if volunteering in volunteeringsOfVolunteer:
+                inscripted = True
+        if volunteer:
+            activities = ActivitieDetailPage.objects.filter(volunteers = volunteer[0])
+            vehicles = volunteer[0].vehicles.all()
+            vehicles = vehicles
+            activities = activities
+            volunteer = volunteer[0]
+            return render(request, "activitie/activitie_listing_page.html",{
+                    "posts":posts,
+                    "current_date":current_date,
+                    "vehicles":vehicles,
+                    "activities":activities,
+                    "volunteer":volunteer,
+                    "inscripted":inscripted,
+            })
     volunteer = None
     return render(request, "activitie/activitie_listing_page.html",{
          "posts":posts,
