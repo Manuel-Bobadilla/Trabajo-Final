8303e1af46b0d0742704f02fe346a12a83c1825b
Manuel-Bobadilla <manuelcba@outlook.com>
2023-09-25 19:30:03 -0300

    modified build.sh file, apt added update

--- build.sh
+++ build.sh
@@ -2,6 +2,15 @@
 # exit on error
 set -o errexit
 
+apt-get update --yes --quiet && apt-get install --yes --quiet --no-install-recommends \
+    build-essential \
+    libpq-dev \
+    libmariadbclient-dev \
+    libjpeg62-turbo-dev \
+    zlib1g-dev \
+    libwebp-dev \
+ && rm -rf /var/lib/apt/lists/*
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
