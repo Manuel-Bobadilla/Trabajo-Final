610e01b343773fb317a9e2dae8c16732854bbed8
Manuel-Bobadilla <manuelcba@outlook.com>
2023-10-02 19:30:36 -0300

    reverse changes. Refs #6

--- mysite/urls.py
+++ mysite/urls.py
@@ -32,17 +32,19 @@ urlpatterns = [
     path("voluntariado/", ViewVolunteering, name="ver_voluntariado"),
 ]
 
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Serve static and media files from development server
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-import debug_toolbar
-urlpatterns = urlpatterns + [
-    path('__debug__/', include('debug_toolbar.urls')),
-] 
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+    urlpatterns = urlpatterns + [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] 
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
