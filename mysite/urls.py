6fd5f9640c518c0c7a8205d4899a3de18ea2efd0
Manuel-Bobadilla <manuelcba@outlook.com>
2023-10-02 17:56:50 -0300

    reverted change. Refs #6

--- mysite/urls.py
+++ mysite/urls.py
@@ -33,18 +33,18 @@ urlpatterns = [
 ]
 
 
-#if settings.DEBUG:
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Serve static and media files from development server
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-import debug_toolbar
-urlpatterns = urlpatterns + [
-    path('__debug__/', include('debug_toolbar.urls')),
-] 
+    import debug_toolbar
+    urlpatterns = urlpatterns + [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] 
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
