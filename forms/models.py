c79e9054c586264fe88273f229ba4aa5ed6a6c65
Manuel-Bobadilla <manuelcba@outlook.com>
2023-10-09 17:46:09 -0300

    added fist and last name forms

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from allauth.account.forms import SignupForm
+from django import forms
 
-# Create your models here.
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=25, label='First Name')
+    last_name = forms.CharField(max_length=25, label='Last Name')
+
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
