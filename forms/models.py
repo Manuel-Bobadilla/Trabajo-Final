2c4a08aad825fd0f71fa6ef260aee26b13eea51f
Manuel-Bobadilla <manuelcba@outlook.com>
2023-10-09 18:03:19 -0300

    placeholder forms fields

--- forms/models.py
+++ forms/models.py
@@ -3,8 +3,14 @@ from allauth.account.forms import SignupForm
 from django import forms
 
 class CustomSignupForm(SignupForm):
-    first_name = forms.CharField(max_length=25, label='First Name')
-    last_name = forms.CharField(max_length=25, label='Last Name')
+    first_name = forms.CharField(max_length=25,
+                                  label='First Name',
+                                  widget=forms.TextInput(attrs={"placeholder":("Nombre"),}),
+                                  )
+    last_name = forms.CharField(max_length=25,
+                                 label='Last Name',
+                                 widget=forms.TextInput(attrs={"placeholder":("Nombre"),}),
+                                 )
 
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
