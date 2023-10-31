3d76d6b18199bad1e390ad5d70c2299df2282db0
Manuel-Bobadilla <manuelcba@outlook.com>
2023-10-31 17:30:45 -0300

    improve users admin

--- users/admin.py
+++ users/admin.py
@@ -8,7 +8,7 @@ class UserAdmin(ModelAdmin):
     menu_icon = "placeholder"
     add_to_settings_menu = False
     exclude_from_explorer = False
-    list_display = ("user", "address", "phone",)
-    search_fields = ("user",)
+    list_display = ("user", "address", "phone", "university", "university_file", "validated")
+    search_fields = ("user__last_name", "user__first_name", "university", "university_file", "dni")
 
 modeladmin_register(UserAdmin)
