40f13fdf7743e5631d6a27900e94c84e13e15084
Manuel-Bobadilla <manuelcba@outlook.com>
2023-12-28 16:47:48 -0300

    reinicio de inscripciones

--- restart/views.py
+++ restart/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from users.models import Volunteer, User
+from vehicles.models import Vehicle
+from volunteerings.models import Volunteering
+from activitie.models import ActivitieDetailPage
 from restart.models import Restart
 import datetime
 
@@ -9,6 +12,20 @@ def RestartYear(request):
 
     if (not user.is_superuser or restart):
         return
+
+    activities = ActivitieDetailPage.objects.all()
+    vehicles = Vehicle.objects.filter(activitie__in = activities)
+    volunteerings = Volunteering.objects.all()
+
+    for vehicle in vehicles:
+        vehicle.activitie = None
+        vehicle.save()
+
+    for activity in activities:
+        activity.volunteers.clear()
+
+    for volunteering in volunteerings:
+        volunteering.volunteers.clear()
 
     restartYear = Restart(date = datetime.date.today())
     restartYear.save()
