7313e2d1a4ad6f828108fc631dced7d17892022f
Manuel-Bobadilla <manuelcba@outlook.com>
2024-01-29 16:49:19 -0300

    correcion inscripcion voluntarios voluntariado

--- volunteerings/models.py
+++ volunteerings/models.py
@@ -5,6 +5,7 @@ class Volunteering(models.Model):
     name = models.CharField(max_length=30, null=False, blank=False)
     description = models.CharField(null=False, blank=False)
     volunteers = models.ManyToManyField(Volunteer, related_name="volunteering")
+    coordinadores = models.ManyToManyField(Volunteer, related_name="coordina")
     image = models.ImageField(help_text="Logo del voluntariado", null=True, blank=False)
 
     def __str__(self):
--- volunteerings/views.py
+++ volunteerings/views.py
@@ -87,6 +87,67 @@ def ViewVolunteersVolunteeing(request):
          "coordinadores":coordinadores,
     })
 
+def ViewCoordinatorsVolunteering(request):
+    #verificar que quien acceda sea el director del vucc
+
+    coordinator = Volunteer.objects.get(Q(user__id=request.user.id) & Q(user__is_superuser=True))
+    volunteering = Volunteering.objects.get(id=request.GET.get("volunteering_id"))
+    volunteeringCoordinators = None
+    restOfCoordinators = None
+
+    volunteeringCoordinators = Volunteer.objects.filter(coordina=volunteering, validated = True, coordinador=True).order_by("user__last_name")
+    restOfCoordinators = Volunteer.objects.exclude(Q(coordina=volunteering) | Q(validated=False) | Q(coordinador=False)).order_by("user__last_name")
+
+    print(volunteeringCoordinators)
+    print(restOfCoordinators)
+
+    return render(request, "volunteerings/coordinators.html",{
+         "volunteering":volunteering,
+         "volunteeringCoordinators":volunteeringCoordinators,
+         "restOfCoordinators":restOfCoordinators,
+    })
+
+#ademas de agregar como coordinador agrega como voluntario
+#al revés no, cuando quita como coordinador no quita como voluntario
+def CoordinatorVolunteering(request):
+    #verificar que quien acceda sea un director del vucc
+    coordinator = Volunteer.objects.get(Q(user__id=request.user.id) & Q(user__is_superuser=True))
+
+    volunteersInscriptedId = set()
+    coordinatorsInscripted = None
+    volunteering = None
+    volunteeringCoordinators = None
+
+    for attendance in request.POST:
+        if attendance != "volunteering_id" and attendance != "csrfmiddlewaretoken":
+            volunteersInscriptedId.add(attendance)
+
+    volunteersInscriptedIdList = list(volunteersInscriptedId)
+
+    coordinatorsInscripted = Volunteer.objects.filter(id__in = volunteersInscriptedIdList, coordinador=True)
+    volunteering = get_object_or_404(Volunteering, id=request.POST.get("volunteering_id"))
+    volunteeringCoordinators = Volunteer.objects.filter(volunteering = volunteering, coordinador=True)
+
+    for volunteer in volunteeringCoordinators:
+        if volunteer.id not in volunteersInscriptedIdList:
+            volunteering.coordinadores.remove(volunteer)
+    
+    for volunteer in coordinatorsInscripted:
+        if not volunteering.coordinadores.filter(id=volunteer.id).exists():
+            volunteering.coordinadores.add(volunteer)
+        if not volunteering.volunteers.filter(id=volunteer.id).exists():
+            volunteering.volunteers.add(volunteer)
+
+    mutable_get = request.GET.copy()
+    mutable_get['volunteering_id'] = request.POST.get("volunteering_id")
+    request.GET = QueryDict(mutable_get.urlencode(), mutable=False)
+
+    url_destino = f'/voluntariados/'
+
+    return redirect(url_destino)
+    
+#si el que se desinscribe es un coordinador hay que sacarlo
+#de la lista de coordinadores del voluntariado
 def InscriptionVolunteering(request):
     #verificar que quien acceda sea un coordinador
     try:
@@ -101,7 +162,7 @@ def InscriptionVolunteering(request):
 
     for attendance in request.POST:
         if attendance != "volunteering_id" and attendance != "csrfmiddlewaretoken" and attendance != "coordinadores":
-            volunteersInscriptedId.add(attendance)
+            volunteersInscriptedId.add(int(attendance))
     volunteersInscriptedIdList = list(volunteersInscriptedId)
 
     if coordinadores == "true":
@@ -124,6 +185,9 @@ def InscriptionVolunteering(request):
                     vehicle[0].save(force_update=True)
 
             volunteering.volunteers.remove(volunteer)
+    
+            if volunteering.coordinadores.filter(id=volunteer.id).exists():
+                volunteering.coordinadores.remove(volunteer)
     
     for volunteer in volunteersInscripted:
         if not volunteering.volunteers.filter(id=volunteer.id).exists():
