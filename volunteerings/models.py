db40b02c233028a5325bd8294b95b6bac0245e3b
Manuel-Bobadilla <manuelcba@outlook.com>
2024-02-03 21:55:16 -0300

    blank true en volunteerings

--- volunteerings/models.py
+++ volunteerings/models.py
@@ -4,8 +4,8 @@ from users.models import Volunteer
 class Volunteering(models.Model):
     name = models.CharField(max_length=30, null=False, blank=False)
     description = models.CharField(null=False, blank=False)
-    volunteers = models.ManyToManyField(Volunteer, related_name="volunteering")
-    coordinadores = models.ManyToManyField(Volunteer, related_name="coordina")
+    volunteers = models.ManyToManyField(Volunteer, related_name="volunteering", blank=True)
+    coordinadores = models.ManyToManyField(Volunteer, related_name="coordina", blank=True)
     image = models.ImageField(help_text="Logo del voluntariado", null=True, blank=False)
 
     def __str__(self):
