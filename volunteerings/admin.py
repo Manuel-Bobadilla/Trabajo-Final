74d07046864ba519e0137197e43d641d8d3c9a71
Manuel-Bobadilla <manuelcba@outlook.com>
2024-02-04 19:32:55 -0300

    en vista voluntariado no muestra los voluntarios ni coordinadores

--- volunteerings/admin.py
+++ volunteerings/admin.py
@@ -1,5 +1,15 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail.admin.panels import FieldPanel
 from .models import Volunteering
+
+class CustomVolunteeringPanel(FieldPanel):
+    def on_form_bound(self):
+        super().on_form_bound()
+        if self.instance:
+            # Si el objeto ya existe (no es nuevo), oculta el campo en el formulario de edición
+            self.form.fields[self.field_name].widget.can_add_related = False
+            self.form.fields[self.field_name].widget.can_change_related = False
+            self.form.fields[self.field_name].widget.can_delete_related = False
 
 class VolunteeringAdmin(ModelAdmin):
     model = Volunteering
@@ -9,5 +19,11 @@ class VolunteeringAdmin(ModelAdmin):
     exclude_from_explorer = False
     list_display = ("name",)
     search_fields = ("name",)
+
+    panels = [
+        CustomVolunteeringPanel('name'),
+        CustomVolunteeringPanel('description'),
+        CustomVolunteeringPanel('image'),
+    ]
 
 modeladmin_register(VolunteeringAdmin)
