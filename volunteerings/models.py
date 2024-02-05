45cdaaba84f641db0ba35c9be369a0d318854008
Manuel-Bobadilla <manuelcba@outlook.com>
2024-02-05 16:29:22 -0300

    descripcion breve voluntariados

--- volunteerings/models.py
+++ volunteerings/models.py
@@ -3,7 +3,8 @@ from users.models import Volunteer
 
 class Volunteering(models.Model):
     name = models.CharField(max_length=30, null=False, blank=False, verbose_name="Nombre voluntariado")
-    description = models.CharField(null=False, blank=False, verbose_name="Breve descripción del voluntariado")
+    description = models.CharField(null=False, blank=False, verbose_name="Descripción del voluntariado")
+    short_description = models.CharField(null=True, blank=True, verbose_name="Breve descripción, para vista en celulares")
     volunteers = models.ManyToManyField(Volunteer, related_name="volunteering", blank=True, verbose_name="Voluntarios del voluntariado")
     coordinadores = models.ManyToManyField(Volunteer, related_name="coordina", blank=True, verbose_name="Coordinadores del voluntariado")
     image = models.ImageField(help_text="Logo del voluntariado", null=True, blank=False, verbose_name="Imagen voluntariado")
