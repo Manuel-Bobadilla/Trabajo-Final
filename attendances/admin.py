5a14ad193fe7cdc6b70d27d22864d2c873cd70e9
Manuel-Bobadilla <manuelcba@outlook.com>
2024-02-07 20:01:44 -0300

    asistencias en panel administracion

--- attendances/admin.py
+++ attendances/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from .models import Attendance
 
-# Register your models here.
+
+class UserAdmin(ModelAdmin):
+    model = Attendance
+    menu_label = "Asistencias"
+    menu_icon = "placeholder"
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("Voluntario", "Actividad", "Fecha",)
+    search_fields = ("volunteer__user__last_name", "volunteer__user__first_name", "date",)
+    
+    def Voluntario(self,obj):
+        return obj.volunteer
+    
+    def Actividad(self,obj):
+        return obj.activity_title
+    
+    def Fecha(self,obj):
+        return obj.date
+
+modeladmin_register(UserAdmin)
