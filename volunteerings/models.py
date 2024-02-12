116bf3f580edb0c90065f64f0f005df8a3d23b76
Manuel-Bobadilla <manuelcba@outlook.com>
2024-02-12 12:18:11 -0300

    retoques muestra voluntariados

--- volunteerings/models.py
+++ volunteerings/models.py
@@ -8,6 +8,8 @@ class Volunteering(models.Model):
     volunteers = models.ManyToManyField(Volunteer, related_name="volunteering", blank=True, verbose_name="Voluntarios del voluntariado")
     coordinadores = models.ManyToManyField(Volunteer, related_name="coordina", blank=True, verbose_name="Coordinadores del voluntariado")
     image = models.ImageField(help_text="Logo del voluntariado", null=True, blank=False, verbose_name="Imagen voluntariado")
+    Whatsapp = models.CharField(help_text="Contacto de Whatsapp para el voluntariado", null=True, blank=True)
+    Instagram = models.CharField(help_text="Contacto de Instagram para el voluntariado", null=True, blank=True)
 
     def __str__(self):
         return self.name
